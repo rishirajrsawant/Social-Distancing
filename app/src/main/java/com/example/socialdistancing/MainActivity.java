eb7cd4b3ad8ad6ba1892f2cd65e771ee86299344
rishirajrsawant <rishiraj.sawant97@gmail.com>
2020-07-06 23:41:51 +0100

    v1.2 removed bg subtraction and contours functionality - implemented HOG descriptor method to detect only people
    to do
    -Find Centers of detected humans
    -Check whether the distance follows social distancing rules

--- app/src/main/java/com/example/socialdistancing/MainActivity.java
+++ app/src/main/java/com/example/socialdistancing/MainActivity.java
@@ -35,6 +35,7 @@ import java.util.List;
 import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
 import static org.opencv.imgproc.Imgproc.RETR_TREE;
 import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.cvtColor;
 import static org.opencv.imgproc.Imgproc.moments;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
         cameraBridgeViewBase.enableFpsMeter();
-        cameraBridgeViewBase.setMaxFrameSize(640, 480);
+        cameraBridgeViewBase.setMaxFrameSize(320, 240);
 
 
 
@@ -102,59 +103,27 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         hogDescriptor = new HOGDescriptor();
         foundLocations = new MatOfRect();
         foundWeights = new MatOfDouble();
-        tempMat = new Mat(frame.rows(), frame.cols(), CvType.CV_8UC3);
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        frame = inputFrame.rgba();
+        frame = inputFrame.gray();
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-
-
-
-
 
         // detector = SimpleBlobDetector.create();
         hogDescriptor.setSVMDetector(HOGDescriptor.getDefaultPeopleDetector());
-        Imgproc.cvtColor(frame, mRgb, Imgproc.COLOR_RGBA2GRAY);
-        mog2.apply(frame, mFGMask, 0.4); //apply() exports a gray image by definition
-        Imgproc.cvtColor(mFGMask, frame, Imgproc.COLOR_GRAY2RGBA);
+//        Imgproc.cvtColor(frame, mRgb, Imgproc.COLOR_RGBA2GRAY);
+//        mog2.apply(frame, mFGMask, 0.4); //apply() exports a gray image by definition
+//        Imgproc.cvtColor(mFGMask, tempMat, Imgproc.COLOR_GRAY2RGBA);
 
-//        detector.detect(mFGMask, keypoints, frame);
-//        int flags = features2d.DRAW_RICH_KEYPOINTS;
-//        features2d.drawKeypoints(mFGMask, keypoints, frame, new Scalar(0,0,255), flags);
+        hogDescriptor.detectMultiScale(frame, foundLocations, foundWeights, 0, new Size(4,4),
+                new Size(8, 8), 1.05);
 
-//        detector.compute(frame, keypoints, descriptors);
+                    Log.i("area798", "a:"+foundLocations);
 
-
-
-        Imgproc.GaussianBlur(mFGMask, blur, new Size(5, 5), 0);
-//      Imgproc.Canny(blur, blur, 80, 100);
-        Imgproc.threshold(blur, thresh, 20, 255, THRESH_BINARY);
-        Imgproc.dilate(thresh, dilate, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
-        Imgproc.findContours(dilate, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-        Imgproc.cvtColor(frame, tempMat, Imgproc.COLOR_RGBA2RGB);
-
-        for ( int contourIdx=0; contourIdx < contours.size(); contourIdx++ )
-        {
-            hogDescriptor.detectMultiScale(tempMat, foundLocations, foundWeights, 1.5, new Size(8,8),
-                    new Size(32, 32), 1.05);
-            // Minimum size allowed for consideration
-            MatOfPoint2f approxCurve = new MatOfPoint2f();
-            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(contourIdx).toArray() );
-            //Processing on mMOP2f1 which is in type MatOfPoint2f
-            double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
-            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
-
-            //Convert back to MatOfPoint
-            MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
-
-
-
-            // Get bounding rect of contour
-            Rect rect = Imgproc.boundingRect(points);
-
-            //double contourArea = Imgproc.contourArea(contours.get(contourIdx));
+                    for(Rect rect : foundLocations.toArray()){
+                        Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
+                    }
 
 
 
@@ -163,36 +132,65 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
 
 
+//        Imgproc.GaussianBlur(frame, blur, new Size(5, 5), 0);
+//////      Imgproc.Canny(blur, blur, 80, 100);
+//          Imgproc.threshold(blur, thresh, 20, 255, THRESH_BINARY);
+//          Imgproc.dilate(thresh, dilate, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
+//          Imgproc.findContours(dilate, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+//
+//        for ( int contourIdx=0; contourIdx < contours.size(); contourIdx++ )
+//        {
+//
+//            hogDescriptor.detectMultiScale(frame, foundLocations, foundWeights, 1.5, new Size(4,4),
+//                   new Size(8, 8), 1.05);
+//            // Minimum size allowed for consideration
+//            MatOfPoint2f approxCurve = new MatOfPoint2f();
+//            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(contourIdx).toArray() );
+//            //Processing on mMOP2f1 which is in type MatOfPoint2f
+//            double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+//            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+//
+//            //Convert back to MatOfPoint
+//            MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
+//
+//
+//
+//            // Get bounding rect of contour
+//            Rect rect = Imgproc.boundingRect(points);
+//
+//            double contourArea = Imgproc.contourArea(contours.get(contourIdx));
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//              if (contourArea < 50){}
+//            else {
+//
+////                List<Moments> mu = new ArrayList<Moments>(contours.size());
+////                for (int i = 0; i < contours.size(); i++) {
+////                    mu.add(i, Imgproc.moments(contours.get(i), false));
+////                    Moments p = mu.get(i);
+////                    int x = (int) (p.get_m10() / p.get_m00());
+////                    int y = (int) (p.get_m01() / p.get_m00());
+////                    Imgproc.circle(frame, new Point(x, y), 4, new Scalar(0, 255, 0));
+////                }
+//            //Log.i("area798", "a:"+contourArea);
+//            Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 3);
+//             }
+//
+//
+//        }
 
-            //  if (contourArea < 100){}
-            //else {
-
-//                List<Moments> mu = new ArrayList<Moments>(contours.size());
-//                for (int i = 0; i < contours.size(); i++) {
-//                    mu.add(i, Imgproc.moments(contours.get(i), false));
-//                    Moments p = mu.get(i);
-//                    int x = (int) (p.get_m10() / p.get_m00());
-//                    int y = (int) (p.get_m01() / p.get_m00());
-//                    Imgproc.circle(frame, new Point(x, y), 4, new Scalar(0, 255, 0));
-//                }
-            //Log.i("area798", "a:"+contourArea);
-            Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 3);
-            // }
-
-        }
 
 
 
 
-//        Imgproc.drawContours(mRgb, contours, -1, new Scalar(0, 255, 0), 2);
-
-//        Imgproc.erode(mFGMask, frame, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
-//        Imgproc.dilate(mFGMask, frame, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
-//        Imgproc.morphologyEx(mFGMask, frame, MORPH_OPEN, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
-//        Imgproc.morphologyEx(mFGMask, frame, MORPH_CLOSE, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
-
-
-//        //System.gc();
+//        System.gc();
         contours.clear();
         return frame;
     }
